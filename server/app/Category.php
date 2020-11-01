f93977b9b9e670c0dc6d0d9c56d3ff8cd2493d7e
KOUDAI <kodai199565@gmail.com>
2020-11-01 13:06:06 +0900

    検索機能修正

--- server/app/Category.php
+++ server/app/Category.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function restaurans()
+    // カテゴりからみて､レストランは複数なので｡Sをつける
+    {
+        return $this->hasMany('App\Restaurant');
+    }
 }
